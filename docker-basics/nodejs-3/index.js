b3cdbc5248293565852a72ecec91e9103ec792d7
Erdem Bozdag <erdembozdg@gmail.com>
2020-12-19 23:11:43 -0800

    Created nodejs-3

---
+++ docker-basics/nodejs-3/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World Node.js')
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
+
